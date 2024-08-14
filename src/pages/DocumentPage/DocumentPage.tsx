128f506da6cefefb23aa6d3d279e8afe23ab4bbf
antisyst <redmiramazan@gmail.com>
2024-08-14 18:49:56 +0400

    fkgkk

--- src/pages/DocumentPage/DocumentPage.tsx
+++ src/pages/DocumentPage/DocumentPage.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import MainLogo from '../../assets/logo.svg';
-import { AppRoot, Section, Image, FixedLayout, Text, InlineButtons } from '@telegram-apps/telegram-ui';
+import { AppRoot, FixedLayout, Text, InlineButtons } from '@telegram-apps/telegram-ui';
 import './DocumentPage.scss';
 import IconInbox from '../../assets/icon_inbox.svg';
 import IconOutBox from '../../assets/icon_outbox.svg';
@@ -9,21 +9,21 @@ const DocumentPage: React.FC = () => {
   return (
     <AppRoot>
       <FixedLayout vertical="top" className="full-document-screen">
-        <Section className="logo-container">
+        <div className="logo-container">
             <img src={MainLogo} alt="App Logo" className="document-logo" />
-        </Section>
-        <Section className="new-contract-section">
+        </div>
+        <div className="new-contract-section">
           <InlineButtons mode="plain" className='docs-buttons'>
             <InlineButtons.Item className='docs-button'>
-              <Image src={IconInbox} alt="Inbox Icon" />
+              <img src={IconInbox} alt="Inbox Icon" />
               <Text weight='3'>Входящие</Text>
             </InlineButtons.Item>
             <InlineButtons.Item className='docs-button'>
-              <Image src={IconOutBox} alt="Outbox Icon" />
+              <img src={IconOutBox} alt="Outbox Icon" />
               <Text weight='3'>Исходящие</Text>
             </InlineButtons.Item>
           </InlineButtons>
-        </Section>
+        </div>
       </FixedLayout>
     </AppRoot>
   );
